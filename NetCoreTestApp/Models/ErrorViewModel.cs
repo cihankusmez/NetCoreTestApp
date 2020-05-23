3d4065d8eb6a500027a03520ef0bd0dca28b94b8
Cihan KÜSMEZ <cihankusmez@gmail.com>
2020-05-23 16:25:45 +0300

    Add project files.

---
+++ NetCoreTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
